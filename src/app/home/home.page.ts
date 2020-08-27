eb5b46b5ab72d887e8fcd4048912bb3562a4207b
pedroferrarezi <60709746+pedroferrarezi@users.noreply.github.com>
2020-08-27 16:04:26 -0300

    animações
    animações

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, Renderer2 } from '@angular/core';
+import { AnimationController, Platform, Animation } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+@ViewChild('blocks') blocks: any;
+@ViewChild('background') background: any;
   public options: Array<any> = [
 
     { icon: 'person-add-outline', text: 'Indicar Amigos'},
@@ -27,6 +29,46 @@ export class HomePage {
         { icon: 'phone-portrait-outline', text: 'Configurações do App'}
       ];
 
-  constructor() {}
+      public initialStep: number = 0;
+      private maxTranslate: number;
+      private animation: Animation;
+
+
+  constructor(
+    private animationCtrl: AnimationController,
+    private platform: Platform,
+    private renderer: Renderer2
+  ) {
+    this.maxTranslate = this.platform.height() - 200;
+  }
+    ngAfterViewInit() {
+      this.createAnimation();
+    }
+
+      toggleBlocks() {
+        this.initialStep = this.initialStep === 0 ? this.maxTranslate : 0;
+       this.animation.direction(this.initialStep === 0 ? 'reverse' : 'normal').play(); 
+
+       this.setBackgroundOpacity();
+      }
+
+    createAnimation() {
+      this.animation = this.animationCtrl.create()
+        .addElement(this.blocks.nativeElement)
+        .duration(300)
+        .fromTo('transform', 'translateY(0)', `translateY(${this.maxTranslate}px)`);
+      }
+      setBackgroundOpacity() {
+
+        this.renderer.setStyle(this.background.nativeElement, 'opacity', this.initialStep === 0 ? '0' : '1')
+      }
+
+fixedBlocks(): boolean {
+  return this.initialStep === this.maxTranslate; 
+  
 
 }
+
+    }
+
+
